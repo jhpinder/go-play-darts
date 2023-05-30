ba46230ba5235add49b2c87cc9c71a5ffeca341f
James Pinder <jhpinder@gmail.com>
2023-05-30 17:49:16 -0400

    decreases score and goes to next turn

--- go-darts/cmd/http/main.go
+++ go-darts/cmd/http/main.go
@@ -6,12 +6,24 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	log "github.com/sirupsen/logrus"
 )
+
+var (
+	scores        []int
+	currentTurn   int
+	numPlayers    = 2
+	startingScore = 301
+)
+
+type playerScores struct {
+	Scores []int
+}
 
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
@@ -21,9 +33,11 @@ func init() {
 }
 
 func main() {
+	initializeGame()
 	log.Info("Logging some darts info")
 	router := mux.NewRouter()
-	router.HandleFunc("/{score}", homeHandler).Methods("GET")
+	router.HandleFunc("/restart", restartGame).Methods("GET")
+	router.HandleFunc("/{score}", scoreTurn).Methods("GET")
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://192.168.50.244:4200"},
@@ -41,18 +55,53 @@ func main() {
 	}
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func scoreTurn(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
 	log.Info(r)
 	log.Info(vars)
 
-	data := map[string]int{
-		"playerOne": 300,
-		"playerTwo": 301,
+	intScore, err := strconv.Atoi(vars["score"])
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	jsonOut, err := json.Marshal(data)
+	scores[currentTurn] -= intScore
+
+	jsonOut, err := json.Marshal(playerScores{
+		Scores: scores,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = fmt.Fprintln(w, string(jsonOut))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if currentTurn+1 == numPlayers {
+		currentTurn = 0
+	} else {
+		currentTurn++
+	}
+}
+
+func initializeGame() {
+	scores = make([]int, numPlayers)
+	currentTurn = 0
+
+	for i := range scores {
+		scores[i] = startingScore
+	}
+
+}
+
+func restartGame(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	initializeGame()
+	jsonOut, err := json.Marshal(playerScores{
+		Scores: scores,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
