6a8b4a6769f928cfb00d1d6ffb626b1ccbf2812c
James Pinder <jhpinder@gmail.com>
2023-05-30 00:51:25 -0400

    returns json with placeholder ints

--- go-darts/cmd/http/main.go
+++ go-darts/cmd/http/main.go
@@ -2,8 +2,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -16,4 +21,38 @@ func init() {
 
 func main() {
 	log.Info("Logging some darts info")
+	router := mux.NewRouter()
+	router.HandleFunc("/{score}", homeHandler).Methods("GET")
+
+	server := http.Server{
+		Handler:     router,
+		Addr:        ":8080",
+		ReadTimeout: time.Minute,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	log.Info(r)
+	log.Info(vars)
+
+	data := map[string]int{
+		"playerOne": 300,
+		"playerTwo": 301,
+	}
+
+	jsonOut, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = fmt.Fprintln(w, string(jsonOut))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go-darts/go.mod
+++ go-darts/go.mod
@@ -2,6 +2,9 @@ module github.com/jhpinder/go-play-darts/go-darts
 
 go 1.20
 
-require github.com/sirupsen/logrus v1.9.2
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.9.2
+)
 
 require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
